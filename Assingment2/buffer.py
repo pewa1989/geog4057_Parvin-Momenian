37f00953c367d27024631c5647cb1bbbc6ceaa30
Pewa1989 <parvin.momenian9@gmail.com>
2025-01-30 17:12:25 -0600

    update

--- Assingment2/buffer.py
+++ Assingment2/buffer.py
@@ -1,6 +1,6 @@
 import arcpy
 arcpy.analysis.Buffer(
-    in_features="C:/Users/pmomen1/Documents/geog4057_Parvin-Momenian/assignments2/no_retail.shp",
+    in_features="no_retail.shp",
     out_feature_class="./retail_buffer.shp",
     buffer_distance_or_field="500 Meters",
     line_side="FULL",
