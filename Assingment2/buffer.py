64ab6fec3594f11a91b6945b1fb291c8bca43184
Pewa1989 <parvin.momenian9@gmail.com>
2025-01-30 19:03:59 -0600

    update

--- Assingment2/buffer.py
+++ Assingment2/buffer.py
@@ -1,7 +1,7 @@
 import arcpy
 arcpy.analysis.Buffer(
     in_features="no_retail.shp",
-    out_feature_class="./retail_buffer.shp",
+    out_feature_class="./retail_buffer_1.shp",
     buffer_distance_or_field="500 Meters",
     line_side="FULL",
     line_end_type="ROUND",
